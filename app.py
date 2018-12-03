6323e0fee5e4ea757ed8f6e9080b0617ff7aced4
etxahun <etxahun.sanchez@gmail.com>
2018-12-03 12:45:00 +0100

    Updated app.py

--- app.py
+++ app.py
@@ -1,89 +1,127 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restplus import Api, Resource, fields
 
-all_books = [
-    {
-        "name": "White Tiger",
-        "author": "Arvind Adiga"
-    },
-    {
-        "name": "Animal Farm",
-        "author": "George Orwell"
-    }
-]
+import os, json
+from os import listdir
+from os.path import isfile, join
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='Book Api', description='An Api for Books')
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint, version='1.1', title='ESB Services API', description='A simple Services API.', doc='/documentation') # to disable SwaggerUI add the following: , doc=False
+app.register_blueprint(blueprint)
 
-book_model = api.model('book', {
-    'name': fields.String('Name of the book.'),
-    'author': fields.String('Name of the author.')
-})
+# Namespace:
+ns = api.namespace('services', description='Service operations')
 
-parser = api.parser()
-parser.add_argument('author', type=str, required=True, help='name of the author', location='form')
+# Model:
+a_service = api.model('Service_List', {'name' : fields.String(required=True, description='The service name.')})
+b_service = api.model('Service', {'name' : fields.String(required=True, description='The service name.'),
+                                  'command' : fields.String(required=True, description='The command to execute.')})
 
-@api.route('/books')
-class AllBooks(Resource):
+#####################
+# Auxiliary Function ###########################################################
+######################
+def filesInDir():
+    # Returns a dictionary of files inside "services/" folder.
+    res = [f for f in listdir('services/') if isfile(join('services/', f))]
+    return res
 
-    @api.marshal_with(book_model, envelope='data')
+def fileNamesInDir():
+    # Returns a dictionary of filenames, without ".json" extension.
+    res = []
+    for f in listdir('services/'):
+        if isfile(join('services/', f)):
+            res.append(f[:-5])
+    return res
+
+def check_service_existence(s_name):
+    res = fileNamesInDir()
+    if s_name in res:
+        return True
+    else:
+        return None
+
+######################
+# Main Program       ###########################################################
+######################
+
+#EndPoint: /services
+@ns.route('/')
+class ServiceList(Resource):
+    '''Shows a list of services and lets you POST to add new services'''
+    # @api.marshal_with(a_service)
+    @ns.marshal_list_with(a_service)
     def get(self):
-        """
-        get all the books
-        """
-        return all_books, 200
+        '''List all services'''
+        onlyfiles = filesInDir()
+        services = []
 
-    @api.expect(book_model)
+        for p in onlyfiles:
+            if p.endswith('.json'):
+                data = {'name': p[:-5]}
+                # res["services"].append(data)
+                services.append(data)
+        print(services)
+        return services, 200
+
+    @ns.expect(b_service)
+    @ns.marshal_with(b_service, code=201)
     def post(self):
-        """
-        add new book to the list
-        """
-        new_book = api.payload
-        all_books.append(new_book)
-        return {'result': 'Book added'}, 201
+        '''Create a new service'''
+        onlyfiles = filesInDir() # List of files in 'services/' folder
+        args = api.payload # Arguments received in POST JSON
+        filename = args['name'] + str('.json') # 'serviceX.json' format file.
 
-@api.route('/author/<author>')
-class Author(Resource):
+        if filename not in onlyfiles:
+            print("File Dir: " + str(filename))
+            with open(os.path.join("services", filename), 'w') as f:
+                json.dump(args, f)
+                return {"msg": "Service added", "service_data": args}, 201
 
-    @api.marshal_with(book_model, envelope='data')
-    def get(self, author):
-        """
-        get all the books by the author
-        """
-        result = [book for book in all_books if book['author'] == author]
-        return result
+        else:
+            return {"msg": "The specified service already exists."}, 422
 
-@api.route('/book/<book_name>')
-class Book(Resource):
+#EndPoint: /services/<string:service_name>
+@ns.route('/<string:service_name>')
+@ns.response(404, 'Service not found')
+class Service(Resource):
+    '''Show a single service item and lets you delete them'''
+    @ns.doc('get_service')
+    @ns.marshal_with(b_service)
+    def get(self, service_name):
+        '''Fetch a given service'''
+        service = check_service_existence(service_name)
+        if service:
+            with open(os.path.join("services", service_name + str('.json'))) as f:
+                data = json.load(f)
+                return data
+        else:
+            return {"error": "Service not found."}, 404
 
-    def get(self, book_name):
-        """
-        get details of particular book
-        """
-        result = [book for book in all_books if book['name'] == book_name]
-        return result
+    @ns.expect(b_service)
+    @ns.marshal_with(b_service)
+    def put(self, service_name):
+        '''Update a service given its service name'''
+        args = api.payload # Arguments received in PUT JSON
+        filename = service_name + str('.json') # 'serviceX.json' format file.
+        if os.path.exists(os.path.join("services", service_name + str('.json'))):
+            with open(os.path.join("services", filename), 'w') as f:
+                json.dump(args, f)
+                return {"msg": "Service updated", "service_data": args}
+        else:
+            return {"error": "Service not found."}, 404
 
-    @api.doc(parser=parser)
-    def put(self, book_name):
-        """
-        Change the book details
-        """
-        args = parser.parse_args()
-        for index, book in enumerate(all_books):
-            if book['name'] == book_name:
-                all_books[index]['author'] = args['author']
-                return book, 201
-        return None, 201
-
-    def delete(self, book_name):
-        """
-        delete the book
-        """
-        for index, book in enumerate(all_books):
-            if book['name'] == book_name:
-                del all_books[index]
-                return {"response": "book deleted"}, 204
-        return None, 404
+    @ns.response(204, 'Service deleted')
+    def delete(self, service_name):
+        '''Delete a service given its service name'''
+        service = check_service_existence(service_name)
+        # print("Service Name: " + str(service_name))
+        # if service:
+        if os.path.exists(os.path.join("services", service_name + str('.json'))):
+            os.remove(os.path.join("services", service_name + str('.json')))
+            return {"message": "Service deleted."}, 204
+        else:
+            return {"error": "Service not found"}, 404
 
 if __name__ == '__main__':
     app.run(debug=True)
